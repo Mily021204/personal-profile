5a61753eb8e2af8d0aaa670a7825daeaee9512fe
Mily021204 <jmquines021204@gmail.com>
2026-06-28 20:10:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-// Fade animation
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-const sections = document.querySelectorAll("section");
+import {
+    getFirestore,
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-window.addEventListener("scroll", () => {
-    sections.forEach(section => {
-        const position = section.getBoundingClientRect().top;
+const firebaseConfig = {
+    apiKey: "AIzaSyDgJhWRdExjibvasAqZtSDI7A2htug3-1Q",
+    authDomain: "personalportfolio-dac96.firebaseapp.com",
+    projectId: "personalportfolio-dac96",
+    storageBucket: "personalportfolio-dac96.firebasestorage.app",
+    messagingSenderId: "950339141544",
+    appId: "1:950339141544:web:93edf7614cba8bbbb7679d"
+};
 
-        if(position < window.innerHeight - 100){
-            section.style.opacity = "1";
-            section.style.transform = "translateY(0)";
-        }
-    });
-});
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-sections.forEach(section => {
-    section.style.opacity = "0";
-    section.style.transform = "translateY(50px)";
-    section.style.transition = "all 0.8s ease";
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", async (e)=>{
+
+    e.preventDefault();
+
+    const name=document.getElementById("name").value;
+    const email=document.getElementById("email").value;
+    const message=document.getElementById("message").value;
+
+    try{
+
+        await addDoc(collection(db,"messages"),{
+
+            name:name,
+            email:email,
+            message:message,
+            date:new Date()
+
+        });
+
+        document.getElementById("status").innerHTML="✅ Message Sent Successfully!";
+
+        form.reset();
+
+    }catch(error){
+
+        document.getElementById("status").innerHTML="❌ Failed to Send.";
+
+        console.log(error);
+
+    }
+
 });
